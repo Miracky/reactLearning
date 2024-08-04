0239414437ad9f46fe406cd85298bb547b706ccc
Miracky <mirac.kaya1017@gmail.com>
2024-08-04 17:07:55 +0300

    6. push
    Styles-Fetch-Axios

---
+++ style-definitions/src/components/A/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from "./styles.module.css"
+
+
+function index() {
+  return (
+    <div className={styles.title} > 
+      A
+    </div>
+  )
+}
+
+export default index
---
+++ style-definitions/src/components/B/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from "./styles.module.css"
+
+function index() {
+  return (
+    <div className={styles.title} >
+      B
+    </div>
+  )
+}
+
+export default index
