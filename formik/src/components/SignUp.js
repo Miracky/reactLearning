aa9093e029a72895761ae4908f978c571ab876c4
Miracky <mirac.kaya1017@gmail.com>
2024-08-08 20:23:40 +0300

    10.push
    Yup and Form validations

--- formik/src/components/SignUp.js
+++ formik/src/components/SignUp.js
@@ -3,42 +3,64 @@ import { useFormik } from "formik";
 import validationSchema from "./validations";
 
 function SignUp() {
-  const { handleChange, values, handleSubmit } = useFormik({
-    initialValues: { email: "", password: "", passwordConfirm: "" },
+  const { handleChange, handleSubmit, handleBlur, values, errors, touched } = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+      passwordConfirm: "",
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
     validationSchema,
   });
 
+  console.log(errors);
+
   return (
-    <div onSubmit={handleSubmit}>
-      <label>Email</label>
-      <input
-        id="email"
-        name="email"
-        placeholder="Email"
-        type="email"
-        values={values.email}
-        onChange={handleChange}
-      />
-      <br /> <br />
-      <label>Password</label>
-      <input
-        id="password"
-        name="password"
-        placeholder="Password"
-        type="password"
-        values={values.password}
-        onChange={handleChange}
-      />
-      <br /> <br />
-      <label>Confirm Password</label>
-      <input
-        id="passwordConfirm"
-        name="passwordConfirm"
-        placeholder="Confirm Password"
-        type="password"
-        values={values.passwordConfirm}
-        onChange={handleChange}
-      />
+    <div>
+      <h1>Sign Up</h1>
+      <form onSubmit={handleSubmit}>
+        <label>Email</label>
+        <input
+          id="email"
+          name="email"
+          placeholder="Email"
+          type="email"
+          values={values.email}
+          onChange={handleChange}
+          onBlur={handleBlur}
+        />
+        {errors.email && touched.email && <div className="error" >{errors.email}</div>}
+        <br /> <br />
+        <label>Password</label>
+        <input
+          id="password"
+          name="password"
+          placeholder="Password"
+          type="password"
+          values={values.password}
+          onChange={handleChange}
+          onBlur={handleBlur}
+        />
+        {errors.password && touched.password && <div className="error">{errors.password}</div>}
+        <br /> <br />
+        <label>Confirm Password</label>
+        <input
+          id="passwordConfirm"
+          name="passwordConfirm"
+          placeholder="Confirm Password"
+          type="password"
+          values={values.passwordConfirm}
+          onChange={handleChange}
+          onBlur={handleBlur}
+        />
+        {errors.passwordConfirm && touched.passwordConfirm && <div className="error">{errors.passwordConfirm}</div>}
+        <br /> <br />
+        <button type="submit">Submit</button>
+        <br /> <br />
+        {JSON.stringify(values)}
+      </form>
     </div>
   );
 }
--- formik/src/components/validations.js
+++ formik/src/components/validations.js
@@ -1,7 +1,9 @@
-import {object, string} from 'yup'
+import {object, ref, string} from 'yup'
 
 const validations= object({
-    email:string().email(),
+    email:string().email('Gecerli bir email giriniz.').required('Zorunlu alan'),
+    password: string().min(5, 'Parolaniz en az 5 karakter olmalidir.').required('Zorunlu alan'),
+    passwordConfirm: string().oneOf([ref('password')],'Parolalar uyusmuyor.').required('Zorunlu alan')
 })
 
 export default validations;
